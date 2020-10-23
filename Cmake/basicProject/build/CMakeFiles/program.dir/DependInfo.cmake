e983a364c5cc9ea27a6c9cf45557072215a07b9f
Breno Queiroz <breno1423@gmail.com>
2020-10-22 23:12:05 -0300

    Core: Learning Cmake

---
+++ Cmake/basicProject/build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/breno/Github/Learning/Cmake/basicProject/src/main.cpp" "/home/breno/Github/Learning/Cmake/basicProject/build/CMakeFiles/program.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/math"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/breno/Github/Learning/Cmake/basicProject/build/src/CMakeFiles/maths.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Cmake/basicProject/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "sum.h"
+using namespace std;
+
+int main()
+{
+	cout << "Hello " << sum(1,1) << endl;
+	return 0;
+}
