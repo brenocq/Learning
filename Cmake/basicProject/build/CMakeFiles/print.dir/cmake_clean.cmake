14f4be719ef91a9d99e1089ad2eef6a803fbac09
Brenocq <breno1423@gmail.com>
2020-10-29 20:50:38 -0300

    Feat: Cmake custom targets

--- Cmake/basicProject/build/CMakeFiles/print.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/print"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/print.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
